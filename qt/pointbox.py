12d79d870fd597c901d406acfa8c54d5e0ffa783
wangan67 <n_3uK!WJ$C>
2020-10-15 12:19:17 +0800

    rename

--- qt/pointbox.py
+++ qt/pointbox.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'test1.ui'
+# Form implementation generated from reading ui file 'pointbox.ui'
 #
 # Created by: PyQt5 UI code generator 5.13.0
 #
